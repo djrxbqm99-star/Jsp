4a0038cb67d68a627bcc1e46c1483ffc6309f666
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-29 17:44:44 +0900

    crud실습

--- ch09/src/main/java/util/SQL.java
+++ ch09/src/main/java/util/SQL.java
@@ -14,4 +14,17 @@ public class SQL {
 	public static final String DELETE_USER1 = "DELETE FROM User1 WHERE userid=?";
 	
 	
+	
+	// User2
+		public static final String INSERT_USER2 = "INSERT INTO User2 VALUES (?,?,?,?)";
+		public static final String SELECT_USER2 = "SELECT * FROM User2 WHERE userid=?";
+		public static final String SELECT_ALL_USER2 = "SELECT * FROM User2";
+		public static final String UPDATE_USER2 = "UPDATE User2 SET "
+													+ "name = ?,"
+													+ "birth = ?,"
+													+ "addr = ? "
+													+ "WHERE userid = ?";
+		public static final String DELETE_USER2 = "DELETE FROM User2 WHERE userid=?";
+	
+	
 }
